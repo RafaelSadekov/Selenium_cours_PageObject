68152fad798d3587cd459aa542ae2a416e9ac619
Rafael <rafael.sadekov@gmail.com>
2022-07-22 20:54:40 +0300

    update

--- test_main_page.py
+++ test_main_page.py
@@ -29,12 +29,5 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page:
         page.should_be_cant_see_product_in_basket_opened_from_main_page()
 
 
-class TestLoginPage():
-    def test_should_be_login_page(browser):
-        link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
-        page = LoginPage(browser, link)
-        page.open()
-        page.should_be_login_url()
-        page.should_be_login_form()
-        page.should_be_register_form()
+
 
--- test_product_page.py
+++ test_product_page.py
@@ -2,9 +2,11 @@ import pytest
 from .pages.product_page import ProductPage
 from .pages.basket_page import BasketPage
 
-@pytest.mark.xfail(reason="Fix")
-@pytest.mark.parametrize('promo_offer',
-                         [i for i in range(10)])
+
+@pytest.mark.need_review
+@pytest.mark.parametrize('promo_offer', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 def test_guest_can_add_product_to_basket(browser, promo_offer):
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
     print(f"opened link: {link}")
@@ -26,11 +28,13 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
         # 2. Переходит в корзину по кнопке в шапке сайта
     page.should_be_cant_see_product_in_basket_opened_from_product_page()
 
+@pytest.mark.need_review
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
+
 
 @pytest.mark.register
 class TestUserAddToBasketFromProductPage():
@@ -42,3 +46,15 @@ class TestUserAddToBasketFromProductPage():
         email = str(time.time()) + "@fakemail.org"
         password = email
         page.register_new_user(str(password))
+
+@pytest.mark.need_review
+    def test_user_can_add_product_to_basket(browser):
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_be_check_avaliable_basket()
+        page.should_be_add_product_in_basket()
+        page.should_be_compare_name_product()
+        page.should_be_compare_cost_product_in_basket()
+        page.should_be_guest_cant_see_success_message()
+
+
