f1c59aa315ea36a8126cf24d6cd368f87983de86
Rafael <rafael.sadekov@gmail.com>
2022-07-22 11:39:19 +0300

    Методы-проверки в Page Object

--- conftest.py
+++ conftest.py
@@ -8,7 +8,7 @@ from selenium.webdriver.chrome.options import Options
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default='chrome',
                      help="Choose browser: chrome or firefox")
-    parser.addoption('--language', action='store', default=None,
+    parser.addoption('--language', action='store', default='en',
                      help="Choose language: es, en, ru, fr")
 
 
@@ -33,5 +33,5 @@ def browser(request):
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
-    print("\nquit browser..")
+    print(f"\nquit browser..")
     browser.quit()
--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,3 @@
-from selenium.webdriver.common.by import By
-from selenium import webdriver
-
 class BasePage():
     def __init__(self, browser, url):
         self.browser = browser
